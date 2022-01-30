44a780a5b8f4e28179ba4399192759380c30ece6
spring-backend-mobdev <enieto@mobdev.cl>
2022-01-30 04:18:26 -0300

    Initial commit

---
+++ src/main/java/cl/mobdev/junit5/testing/Junit5AppApplication.java
@@ -0,0 +1,13 @@
+package cl.mobdev.junit5.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/mobdev/junit5/testing/Junit5AppApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.mobdev.junit5.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
